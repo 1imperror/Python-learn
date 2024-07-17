56beae7a22e43ca651a830df95a50ae550cb01fa
Tony <109649123+1imperror@users.noreply.github.com>
2024-07-17 08:25:55 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+quantity_home_work = 12
+quantity_spent_time = 1.5
+course_name = "Python"
+job_time = (quantity_spent_time/quantity_home_work)
+print("Курс:",course_name, "всего задач:", quantity_home_work,"затрачено часов:", quantity_spent_time,"среднее время выполнения", job_time)
