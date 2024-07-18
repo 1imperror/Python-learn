53e08702c8f929876b6ab4fcdd4482e7964fd3ed
Tony <109649123+1imperror@users.noreply.github.com>
2024-07-18 15:20:12 +0700

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,7 @@ name = "Anton"
 print("Name:", name)
 age = 30
 print("Age:", age)
-new_age = (age + 2)
-print("New Age:", new_age)
+age = age + 2
+print("New Age:", age)
 is_student = True
 print("Is Student:", is_student)
