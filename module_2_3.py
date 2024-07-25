c31a1573908c5fad8c748e10835c08a270f00da2
Tony <109649123+1imperror@users.noreply.github.com>
2024-07-25 20:40:04 +0700

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -3,11 +3,11 @@ i = 0
 while i < len(my_list):
     if my_list[i] < 0:
         break
-    if my_list[i] > 1:
+    if my_list[i] > 0:
         print(my_list[i])
         i += 1
-    if my_list[i] == 0:
-         i += 1
+    elif my_list[i] == 0:
+        i += 1
 
 
 
