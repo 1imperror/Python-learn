de6e9edd40908bca3b76dcb50b6364a33cb4c715
Tony <109649123+1imperror@users.noreply.github.com>
2024-07-29 20:18:09 +0700

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,7 +1,11 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
+
+
 def string_info(string):
     count_calls()
     r = len(string)
@@ -9,6 +13,8 @@ def string_info(string):
     s = string.upper()
     gg = (r, s, d)
     return gg
+
+
 def is_contains(string, list_to_search):
     count_calls()
     new_list_to_search = [i.casefold() for i in list_to_search]
@@ -17,6 +23,8 @@ def is_contains(string, list_to_search):
         return True
     else:
         return False
+
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
