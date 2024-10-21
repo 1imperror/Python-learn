f9d8bb39bfebec7fff82914203cde9af3ad0daac
Tony <109649123+1imperror@users.noreply.github.com>
2024-10-21 21:31:48 +0700

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,57 @@
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.command import Command
+from aiogram.filters.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+import asyncio
+
+API = ''
+bot = Bot(token=API)
+dp = Dispatcher(storage=MemoryStorage())
+
+router = Router()
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@router.message(F.text == 'Calories')
+async def set_age(message, state: FSMContext):
+    await message.answer('Введите свой возраст:')
+    await state.set_state(UserState.age)
+
+
+@router.message(UserState.age)
+async def set_growth(message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await state.set_state(UserState.growth)
+
+
+@router.message(UserState.growth)
+async def set_weight(message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await state.set_state(UserState.weight)
+
+
+@router.message(UserState.weight)
+async def send_calories(message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    form = (10 * int(data['weight'])) + (6.25 * int(data['growth']) - (5 * int(data['age'])))
+    await message.answer(f'Ваша норма калорий {form}')
+    await state.clear()
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    dp = Dispatcher()
+    dp.include_router(router)
+    asyncio.run(main())
