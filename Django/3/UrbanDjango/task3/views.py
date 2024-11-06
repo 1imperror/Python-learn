dc4a80c1ccd20d64589e55ea50f2f2093e2f7ce0
Tony <109649123+1imperror@users.noreply.github.com>
2024-11-06 16:18:49 +0700

    Update views.py

--- Django/3/UrbanDjango/task3/views.py
+++ Django/3/UrbanDjango/task3/views.py
@@ -3,12 +3,45 @@ from django.shortcuts import render
 
 # Create your views here.
 def platform_page(request):
-    return render(request, 'platform.html')
+    main_head = 'Главная страница'
+    half_head = 'Главная'
+    shop = 'Магазин'
+    cart = 'Корзина'
+    context = {
+            'main': main_head,
+            'half': half_head,
+            'shop': shop,
+            'cart': cart
+
+    }
+    return render(request, 'platform.html', context)
 
 
 def games_page(request):
-    return render(request, 'games.html')
+    main_head = 'Игры'
+    atom = 'Atomic Heart'
+    cyber = 'Cyberpunk 2077'
+    pay = 'PayDay 2'
+    back = 'Вернуться обратно'
+    bye = 'Купить'
+    context = {
+            'main': main_head,
+            'atom': atom,
+            'cyber': cyber,
+            'pay': pay,
+            'back': back,
+            'bye': bye
+
+    }
+    return render(request, 'games.html', context)
 
 
 def cart_page(request):
-    return render(request, 'cart.html')
+    main_head = 'Извините, ваша корзина пуста'
+    back = 'Вернуться обратно'
+    context = {
+        'main': main_head,
+        'back': back
+
+    }
+    return render(request, 'cart.html', context)
